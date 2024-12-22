5436f041c8e96c93b9c43099173e48a367306243
kumneger <kumnegerwondimu01@gmail.com>
2024-12-22 15:05:20 +0300

    Add TimerTask features diff
    Add Clear, IsCleared, Add methods and bool Cleared field
    to TimerTask. Improve Schedule method with task id.
    Update timers.go with GetTask function and
    clear/set Timeout/Interval improvements.
    Update MakeJSRequest to MakeJSRequestObj.

--- pkg/timer/timers.go
+++ pkg/timer/timers.go
@@ -1,47 +1,79 @@
 package timer
 
 import (
+	"fmt"
+
 	eventloop "github.com/kumneger0/tibebjs/pkg/eventloop"
 	v8 "rogchap.com/v8go"
 )
 
+const BASE_TIMEOUT_ID = 100
+const BASE_INTERVAL_ID = 1000
+
 func setTimeout(info *v8.FunctionCallbackInfo) *v8.Value {
 	callback, _ := info.Args()[0].AsFunction()
+	id := BASE_TIMEOUT_ID + len(eventloop.TimerTaskQueue)
+	eventloop.Mu.Lock()
+	defer eventloop.Mu.Unlock()
 	eventloop.TimerTaskQueue = append(eventloop.TimerTaskQueue, eventloop.TimerTask{
 		Callback: callback,
 		Context:  info.Context(),
+		Id:       id,
 	})
-	eventloop.Schedule(info, false)
-  	return v8.Undefined(info.Context().Isolate())
+	eventloop.Schedule(info, false, id)
+	idToReturnToJs, err := v8.NewValue(info.Context().Isolate(), float64(id))
+	if err != nil {
+		fmt.Println("error creating id to return to js", err.Error())
+	}
+	return idToReturnToJs
 }
 
-func clearTImeout(info *v8.FunctionCallbackInfo) *v8.Value {
-    //TODO: implement clearTimeout
+func clearTimeout(info *v8.FunctionCallbackInfo) *v8.Value {
+	fmt.Println("clearing timeout")
+	id := int(info.Args()[0].Int32())
+	task, err := eventloop.GetTask(id)
+	if err != nil {
+		return v8.Undefined(info.Context().Isolate())
+	}
+	task.Clear()
 	return v8.Undefined(info.Context().Isolate())
 }
 
 func setInterval(info *v8.FunctionCallbackInfo) *v8.Value {
 	callback, _ := info.Args()[0].AsFunction()
+	id := BASE_INTERVAL_ID + len(eventloop.TimerTaskQueue)
+	eventloop.Mu.Lock()
+	defer eventloop.Mu.Unlock()
 	eventloop.TimerTaskQueue = append(eventloop.TimerTaskQueue, eventloop.TimerTask{
 		Callback: callback,
 		Context:  info.Context(),
+		Id:       id,
 	})
-	eventloop.Schedule(info, true)
-	return v8.Undefined(info.Context().Isolate())
+	eventloop.Schedule(info, true, id)
+	idToReturnToJs, err := v8.NewValue(info.Context().Isolate(), float64(id))
+	if err != nil {
+		fmt.Println("error creating id to return to js", err.Error())
+	}
+	return idToReturnToJs
 }
 
 func clearInterval(info *v8.FunctionCallbackInfo) *v8.Value {
-	//TODO: implement clearInterval
+	fmt.Println("clearing interval")
+	id := int(info.Args()[0].Int32())
+	task, err := eventloop.GetTask(id)
+	if err != nil {
+		return v8.Undefined(info.Context().Isolate())
+	}
+	task.Clear()
 	return v8.Undefined(info.Context().Isolate())
 }
-
 
 var TimerFunctions = []struct {
 	Name string
 	Fn   func(*v8.FunctionCallbackInfo) *v8.Value
 }{
 	{"setTimeout", setTimeout},
-	{"clearTimeout", clearTImeout},
+	{"clearTimeout", clearTimeout},
 	{"setInterval", setInterval},
 	{"clearInterval", clearInterval},
 }
